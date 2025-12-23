965e349a96f842c8e01f1edd77bc6941b979e498
Floods <80289503+TheFlood424K@users.noreply.github.com>
2025-12-23 12:11:33 -0500

    Refactor title packet handling to use Bukkit API

--- src/main/java/ro/deiutzblaxo/Purgatory/Spigot/Titles/Spigot1_16.java
+++ src/main/java/ro/deiutzblaxo/Purgatory/Spigot/Titles/Spigot1_16.java
@@ -1,33 +1,27 @@
-//    Purgatory , a ban system for servers of Minecraft
-//    Copyright (C) 2020  Deiutz
+// Purgatory , a ban system for servers of Minecraft
+// Copyright (C) 2020  Deiutz
 //
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package ro.deiutzblaxo.Purgatory.Spigot.Titles;
 
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-import net.minecraft.server.v1_16_R2.IChatBaseComponent.ChatSerializer;
-import net.minecraft.server.v1_16_R2.PacketPlayOutTitle;
-import net.minecraft.server.v1_16_R2.PacketPlayOutTitle.EnumTitleAction;
-
 public class Spigot1_16 {
-	public void Packet1_16(Player player, String titleString) {
-		String titlestring = ChatColor.translateAlternateColorCodes('&', titleString);
-		PacketPlayOutTitle title = new PacketPlayOutTitle(EnumTitleAction.TITLE,
-				ChatSerializer.a("{\"text\":\"" + titlestring + "\"}"), 1, 20, 20);
-		((CraftPlayer) player).getHandle().playerConnection.sendPacket(title);
-	}
+    public void Packet1_16(Player player, String titleString) {
+        String titlestring = ChatColor.translateAlternateColorCodes('&', titleString);
+        // Use modern Bukkit API for titles (works on 1.11+)
+        player.sendTitle(titlestring, "", 1, 20, 20);
+    }
 }
