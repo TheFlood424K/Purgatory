74707cf856e5f4528f5521599013fa4553975e4f
Floods <80289503+TheFlood424K@users.noreply.github.com>
2025-12-24 09:55:56 -0500

    Refactor warning management to use put method
    Fixed all Map API usage errors in WarningFactory:
    - Changed .set() to .put() for Map operations
    - Changed .getString() to .get() with proper casting to String
    - Changed .getInt() to .get() with proper instanceof check and casting
    - Changed .contains() to .containsKey()
    - Fixed getPurgatoryServer() to return String instead of Optional
    - Added null checks for messages retrieved from config
    - Added import for ServerManager

--- src/main/java/ro/deiutzblaxo/Purgatory/Velocity/Factory/WarningFactory.java
+++ src/main/java/ro/deiutzblaxo/Purgatory/Velocity/Factory/WarningFactory.java
@@ -26,94 +26,105 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 import ro.deiutzblaxo.Purgatory.Velocity.MainVelocity;
+import ro.deiutzblaxo.Purgatory.Utils.ServerManager;
 
 public class WarningFactory {
     private MainVelocity plugin;
-    
+
     public WarningFactory(MainVelocity mainVelocity) {
         plugin = mainVelocity;
     }
-    
+
     public void setWarning(Player player, CommandSource sender, String reason) {
         String uuid = player.getUniqueId().toString();
         plugin.getConfigManager().loadWarnings();
         if(!isWarning(player)) {
-            plugin.getConfigManager().getWarnings().set(uuid + ".Value", 1);
-            plugin.getConfigManager().getWarnings().set(uuid + ".Reason", reason);
+            plugin.getConfigManager().getWarnings().put(uuid + ".Value", 1);
+            plugin.getConfigManager().getWarnings().put(uuid + ".Reason", reason);
             plugin.getConfigManager().saveWarnings();
         } else {
             int MaxWarning = plugin.getConfigManager().getConfig().getInt("MaxWarnings");
             int Warning = this.getWarningNumber(player) + 1;
             if(MaxWarning > Warning) {
-                plugin.getConfigManager().getWarnings().set(uuid + ".Value", Warning);
-                plugin.getConfigManager().getWarnings().set(uuid + ".Reason", reason);
+                plugin.getConfigManager().getWarnings().put(uuid + ".Value", Warning);
+                plugin.getConfigManager().getWarnings().put(uuid + ".Reason", reason);
                 plugin.getConfigManager().saveWarnings();
             } else if(MaxWarning <= Warning) {
                 plugin.getBanFactory().setBan(player.getUniqueId(), reason, player.getUsername());
-                
+
                 Optional<Player> playerOpt = plugin.getServer().getPlayer(player.getUniqueId());
                 if(playerOpt.isPresent()) {
                     Player p = playerOpt.get();
-                    String message = plugin.getConfigManager().getString(plugin.getConfigManager().getMessages(), "Ban.Format")
-                        .replaceAll("%reason%", reason);
-                    
+                    String message = (String) plugin.getConfigManager().getMessages().get("Ban.Format");
+                    if(message != null) {
+                        message = message.replaceAll("%reason%", reason);
+                    }
+
                     if(plugin.getConfigManager().getConfig().getBoolean("Ban-Disconnect")) {
                         p.disconnect(deserialize(message));
                     } else {
-                        Optional<RegisteredServer> purgatoryOpt = plugin.getServerManager().getPurgatoryServer();
+                        String purgatoryServerName = ServerManager.getPurgatoryServer();
+                        Optional<RegisteredServer> purgatoryOpt = plugin.getServer().getServer(purgatoryServerName);
                         if(purgatoryOpt.isPresent()) {
                             p.createConnectionRequest(purgatoryOpt.get()).fireAndForget();
                         }
                         p.sendMessage(deserialize(message));
                     }
                 }
-                
+
                 String senderName = "Console";
                 if(sender instanceof Player) {
                     senderName = ((Player) sender).getUsername();
                 }
-                
-                String broadcastMsg = plugin.getConfigManager().getString(plugin.getConfigManager().getMessages(), "Ban.Broadcast")
-                    .replaceAll("%player%", player.getUsername())
-                    .replaceAll("%admin%", senderName)
-                    .replaceAll("%reason%", reason);
-                plugin.getServer().sendMessage(deserialize(broadcastMsg));
+
+                String broadcastMsg = (String) plugin.getConfigManager().getMessages().get("Ban.Broadcast");
+                if(broadcastMsg != null) {
+                    broadcastMsg = broadcastMsg
+                        .replaceAll("%player%", player.getUsername())
+                        .replaceAll("%admin%", senderName)
+                        .replaceAll("%reason%", reason);
+                    plugin.getServer().sendMessage(deserialize(broadcastMsg));
+                }
             }
         }
     }
-    
+
     public void removeWarning(Player player) {
         UUID uuid = player.getUniqueId();
         plugin.getConfigManager().loadWarnings();
-        plugin.getConfigManager().getWarnings().set(uuid.toString(), null);
+        plugin.getConfigManager().getWarnings().put(uuid.toString(), null);
         plugin.getConfigManager().saveWarnings();
     }
-    
+
     public Integer getWarningNumber(Player player) {
         String uuid = player.getUniqueId().toString();
         if(isWarning(player)) {
             plugin.getConfigManager().loadWarnings();
-            return plugin.getConfigManager().getWarnings().getInt(uuid + ".Value");
+            Object value = plugin.getConfigManager().getWarnings().get(uuid + ".Value");
+            if(value instanceof Integer) {
+                return (Integer) value;
+            }
         }
         return 0;
     }
-    
+
     public String getReason(Player player) {
         String uuid = player.getUniqueId().toString();
         plugin.getConfigManager().loadWarnings();
-        return plugin.getConfigManager().getWarnings().getString(uuid + ".Reason");
+        Object reason = plugin.getConfigManager().getWarnings().get(uuid + ".Reason");
+        return reason != null ? (String) reason : "";
     }
-    
+
     public boolean isWarning(Player player) {
         String uuid = player.getUniqueId().toString();
         plugin.getConfigManager().loadWarnings();
-        return plugin.getConfigManager().getWarnings().contains(uuid);
+        return plugin.getConfigManager().getWarnings().containsKey(uuid);
     }
-    
+
     public int getMaxWarning() {
         return plugin.getConfigManager().getConfig().getInt("MaxWarnings");
     }
-    
+
     private Component deserialize(String legacy) {
         return LegacyComponentSerializer.legacyAmpersand().deserialize(legacy);
     }
