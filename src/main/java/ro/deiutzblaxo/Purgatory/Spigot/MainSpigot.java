c82cb3e637231348e2a9736abd1ba1cc87881960
Floods <80289503+TheFlood424K@users.noreply.github.com>
2025-12-25 17:10:59 -0500

    Fix MainSpigot.java: Remove deleted command imports and fix onEnable structure
    - Removed imports for InfoCommand, WarningCommand, tpoCommand
    - Fixed onEnable() method structure with proper if(!isBungeeEnabled()) block
    - Updated command registrations to use new command names (purgeban, free, trolltp)
    - Removed registrations for deleted commands (info, warning, tpo)

--- src/main/java/ro/deiutzblaxo/Purgatory/Spigot/MainSpigot.java
+++ src/main/java/ro/deiutzblaxo/Purgatory/Spigot/MainSpigot.java
@@ -1,18 +1,18 @@
-//    Purgatory , a ban system for servers of Minecraft
-//    Copyright (C) 2020  Deiutz
+// Purgatory , a ban system for servers of Minecraft
+// Copyright (C) 2020  Deiutz
 //
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package ro.deiutzblaxo.Purgatory.Spigot;
 
 import java.io.BufferedReader;
@@ -47,11 +47,11 @@ import org.bukkit.scheduler.BukkitRunnable;
 import ro.deiutzblaxo.Purgatory.Spigot.API.ScoreBoardAPI;
 import ro.deiutzblaxo.Purgatory.Spigot.Commands.BanCommand;
 import ro.deiutzblaxo.Purgatory.Spigot.Commands.CheatersCommand;
-import ro.deiutzblaxo.Purgatory.Spigot.Commands.InfoCommand;
 import ro.deiutzblaxo.Purgatory.Spigot.Commands.PurgatoryCommand;
 import ro.deiutzblaxo.Purgatory.Spigot.Commands.PurgeCommand;
 import ro.deiutzblaxo.Purgatory.Spigot.Commands.TempBanCommand;
 import ro.deiutzblaxo.Purgatory.Spigot.Commands.TrollCommand;
+import ro.deiutzblaxo.Purgatory.Spigot.Commands.tppCommand;
 import ro.deiutzblaxo.Purgatory.Spigot.Events.BungeeCommunication;
 import ro.deiutzblaxo.Purgatory.Spigot.Events.JustBungeeEvents;
 import ro.deiutzblaxo.Purgatory.Spigot.Events.JustSpigotEvents;
@@ -69,6 +69,7 @@ import ro.deiutzblaxo.Purgatory.Spigot.Troll.Players;
 import ro.deiutzblaxo.Purgatory.Spigot.Troll.Trolls;
 
 public class MainSpigot extends JavaPlugin implements Listener {
+
 	private static MainSpigot instance;
 	private ConfigManager configmanager;
 	private BanFactory BanFactory;
@@ -79,7 +80,7 @@ public class MainSpigot extends JavaPlugin implements Listener {
 	private ScoreBoardAPI ScoreBoardAPI;
 	private BungeeCommunication BungeeCommunication;
 	private TitleManager TitleManager;
-
+	
 	@SuppressWarnings("unused")
 	@Override
 	public void onEnable() {
@@ -91,55 +92,57 @@ public class MainSpigot extends JavaPlugin implements Listener {
 		TaskFactory = new TaskFactory(this);
 		ScoreBoardAPI = new ScoreBoardAPI();
 		TitleManager = new TitleManager(this);
-
-		//TODO SUGGESTIONS :  ip bans , make banned chat different from normal chat , Add the ability to give weapons to banned players when they are banned and when they respawn
+		//TODO SUGGESTIONS : ip bans , make banned chat different from normal chat , Add the ability to give weapons to banned players when they are banned and when they respawn
 		//TODO data base using MYSQL
-
-
 		new Metrics(this);
 		//setting up the commands
 		loadCommandMap();
 		this.commandMap.register("purgatory", new CheatersCommand(this.getConfig().getString("Command.Cheaters") , this));
 		this.commandMap.register("purgatory", new TrollCommand(this.getConfig().getString("Command.Troll") , this));
-        this.commandMap.register("purgeban", new PurgatoryCommand("purgeban" , this));		this.commandMap.register("purgatry", new ScoreboardCommand("tasks", this ));
+		this.commandMap.register("purgeban", new PurgatoryCommand("purgeban" , this));
+		this.commandMap.register("purgatry", new ScoreboardCommand("tasks", this ));
+		
+		if(!isBungeeEnabled()) {
 			WorldManager = new WorldManager(this);
 			getServer().getPluginManager().registerEvents(new JustSpigotEvents(this), this);
-            this.commandMap.register("purgeban", new BanCommand(this.getConfig().getString("Command.Ban"), this));			this.commandMap.register("purgatory", new InfoCommand(this.getConfig().getString("Command.Info") , this));
-            this.commandMap.register("free", new PurgeCommand(this.getConfig().getString("Command.Purge") , this));			this.commandMap.register("purgatory", new TempBanCommand(this.getConfig().getString("Command.TempBan") , this));
-            this.commandMap.register("trolltp", new tppCommand(this.getConfig().getString("Command.tpp") , this));			this.commandMap.register("purgatory", new tpoCommand(this.getConfig().getString("Command.tpo") , this));
-
+			this.commandMap.register("purgeban", new BanCommand(this.getConfig().getString("Command.Ban"), this));
+			this.commandMap.register("free", new PurgeCommand(this.getConfig().getString("Command.Purge") , this));
+			this.commandMap.register("purgatory", new TempBanCommand(this.getConfig().getString("Command.TempBan") , this));
+			this.commandMap.register("trolltp", new tppCommand(this.getConfig().getString("Command.tpp") , this));
 		}
-
+		
 		getServer().getPluginManager().registerEvents(new BreakTask(this), this);
 		getServer().getPluginManager().registerEvents(new PlaceTask(this), this);
 		getServer().getPluginManager().registerEvents(new KillTask(this), this);
 		getServer().getPluginManager().registerEvents(new LevelUpTask(this), this);
 		getServer().getPluginManager().registerEvents(new Players(this), this);
 		getServer().getPluginManager().registerEvents(new Trolls(this), this);
+		
 		if(Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
 			new PlaceHolderHooker(this).register();
 			Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&7[&aPurgatory&7]&0 PlaceHolderAPI have been hooked!"));
 		}
+		
 		if(Bukkit.getPluginManager().isPluginEnabled("Citizens")) {
 			getServer().getPluginManager().registerEvents(new CitizensHooker(this), this);
 			Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&7[&aPurgatory&7]&0 Citizens have been hooked!"));
 		}
+		
 		Cooldowns();
 		getBanFactory().EnableTempBan();
 		updateCheckerConsole(this, "&7[&aPurgatory&7]", 65838);
 		getServer().getPluginManager().registerEvents(this, this);
 		getConfigManager().convertBaseData5_0();
+		
 		if(isBungeeEnabled()) {
 			this.getServer().getMessenger().registerOutgoingPluginChannel(this, "purgatory:main");
 			this.getServer().getMessenger().registerIncomingPluginChannel(this, "purgatory:main",
 					new BungeeCommunication(this));
 			setBungeeCommunication(new BungeeCommunication(this));
 			getServer().getPluginManager().registerEvents(new JustBungeeEvents(this), this);
-
 		}
 	}
-
-
+	
 	@Override
 	public void onDisable() {
 		System.out.print("Temp ban system shutdown...");
@@ -153,17 +156,16 @@ public class MainSpigot extends JavaPlugin implements Listener {
 		}
 		getConfigManager().saveBanDataBase();
 		System.out.print("Temp ban system has been shutdown...");
-
 	}
+	
 	public Boolean isBungeeEnabled() {
 		return this.getConfig().getBoolean("BungeeCord");
 	}
-
+	
 	private void loadCommandMap() {
 		try {
 			if (Bukkit.getPluginManager() instanceof SimplePluginManager) {
 				Field f = SimplePluginManager.class.getDeclaredField("commandMap");
-
 				f.setAccessible(true);
 				this.commandMap = (CommandMap) f.get(Bukkit.getPluginManager());
 			}
@@ -177,6 +179,7 @@ public class MainSpigot extends JavaPlugin implements Listener {
 			e.printStackTrace();
 		}
 	}
+	
 	public void updateCheckerConsole(Plugin plugin, String prefix, Integer ResourceNumber) {
 		PluginDescriptionFile pdffile = plugin.getDescription();
 		try {
@@ -197,7 +200,6 @@ public class MainSpigot extends JavaPlugin implements Listener {
 						ChatColor.translateAlternateColorCodes('&', prefix + "&8Don`t forget to rate our plugin!"));
 				Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
 						"&8&m--------------------------------------------------------------------------------------"));
-
 			}
 		} catch (Exception ex) {
 			Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',
@@ -208,6 +210,7 @@ public class MainSpigot extends JavaPlugin implements Listener {
 					"&8&m--------------------------------------------------------------------------------------"));
 		}
 	}
+	
 	@EventHandler(priority = EventPriority.LOWEST , ignoreCancelled = true)
 	public void updateCheckerJoin(PlayerJoinEvent event) {
 		try {
@@ -216,10 +219,9 @@ public class MainSpigot extends JavaPlugin implements Listener {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public void updateCheckerPlayer(Plugin plugin, Player player, String prefix, Integer ResourceNumber)
 			throws MalformedURLException, IOException {
-
 		PluginDescriptionFile pdffile = plugin.getDescription();
 		HttpURLConnection con = (HttpURLConnection) new URL(
 				"https://api.spigotmc.org/legacy/update.php?resource=" + ResourceNumber).openConnection();
@@ -228,7 +230,6 @@ public class MainSpigot extends JavaPlugin implements Listener {
 		con.setReadTimeout(timed_out);
 		String latestversion = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
 		if ((latestversion.length() <= 100) && (!pdffile.getVersion().equals(latestversion))) {
-
 			if ((player.isOp()) && (!pdffile.getVersion().equals(latestversion))) {
 				player.sendMessage(ChatColor.translateAlternateColorCodes('&',
 						"&8&m-----------------------------------------------------"));
@@ -243,28 +244,35 @@ public class MainSpigot extends JavaPlugin implements Listener {
 			}
 		}
 	}
+	
 	public static MainSpigot getInstance() {
 		return instance;
 	}
+	
 	public ConfigManager getConfigManager() {
 		return configmanager;
 	}
+	
 	public BanFactory getBanFactory() {
 		return BanFactory;
 	}
+	
 	public WarningFactory getWarningFactory() {
 		return WarningFactory;
 	}
+	
 	public TaskFactory getTaskFactory() {
 		return TaskFactory;
 	}
+	
 	public WorldManager getWorldManager() {
 		return WorldManager;
 	}
+	
 	public ro.deiutzblaxo.Purgatory.Spigot.API.ScoreBoardAPI getScoreBoardAPI() {
 		return ScoreBoardAPI;
 	}
-
+	
 	public ArrayList<HashMap<UUID,Integer>> Trolls = new ArrayList<HashMap<UUID,Integer>>();
 	public HashMap<UUID,Integer> SmokeScreen = new HashMap<UUID,Integer>();
 	public HashMap<UUID,Integer> SmokeScreen_Effect = new HashMap<UUID,Integer>();
@@ -287,9 +295,8 @@ public class MainSpigot extends JavaPlugin implements Listener {
 	public HashMap<UUID,Integer> Slow = new HashMap<UUID,Integer>();
 	public HashMap<UUID,Integer> JumpBoost = new HashMap<UUID,Integer>();
 	public HashMap<UUID,Integer> Speed = new HashMap<UUID,Integer>();
-
 	public HashMap<UUID,Integer> TempBan = new HashMap<UUID,Integer>();
-
+	
 	public void Cooldowns() {
 		Trolls.add(SmokeScreen);
 		Trolls.add(Burn);
@@ -308,7 +315,7 @@ public class MainSpigot extends JavaPlugin implements Listener {
 		Trolls.add(Slow);
 		Trolls.add(JumpBoost);
 		Trolls.add(Speed);
-
+		
 		new BukkitRunnable() {
 			@Override
 			public void run() {
@@ -321,52 +328,39 @@ public class MainSpigot extends JavaPlugin implements Listener {
 								test.remove(uuid);
 							} else if (timeleft < 0) {
 								test.remove(uuid);
-
 							} else {
 								test.replace(uuid, timeleft - 1);
 							}
 						}
+					}
 				}
-				}
-
-
 			}
 		}.runTaskTimer(this, 0, 20);
-
+		
 		new BukkitRunnable() {
-
-
 			@Override
 			public void run() {
 				if(SmokeScreen_Effect.isEmpty()) {return; }
-
-
 				for(UUID uuid : SmokeScreen_Effect.keySet()) {
 					int timeleft = SmokeScreen_Effect.get(uuid);
 					if (timeleft == 0) {
 						SmokeScreen_Effect.remove(uuid);
 					} else if (timeleft < 0) {
 						SmokeScreen_Effect.remove(uuid);
-
 					} else {
 						if(!instance.getServer().getPlayer(uuid).isOnline() || instance.getServer().getPlayer(uuid) == null){SmokeScreen_Effect.remove(uuid);return;}
 						SmokeScreen_Effect.replace(uuid, timeleft - 5);
 						Player player = instance.getServer().getPlayer(uuid);
 						player.getWorld().spawnParticle(Particle.EXPLOSION, player.getLocation(), 20);
 					}
-
 				}
 			}
-
-
-
 		}.runTaskTimer(this, 0, 5);
+		
 		new BukkitRunnable() {
-
 			@Override
 			public void run() {
 				if(Burn_Effect.isEmpty()) {return;}
-
 				for(Location uuid : Burn_Effect.keySet()) {
 					int timeleft = Burn_Effect.get(uuid);
 					if (timeleft == 0) {
@@ -378,52 +372,39 @@ public class MainSpigot extends JavaPlugin implements Listener {
 					} else {
 						Burn_Effect.replace(uuid, timeleft - 1);
 					}
-
 				}
-
 			}
-
 		}.runTaskTimer(this, 0, 20);
-
+		
 		new BukkitRunnable() {
-
 			@Override
 			public void run() {
 				if(Lag_Effect.isEmpty()) {return;}
-
 				for (UUID uuid : Lag_Effect.keySet()) {
-
 					Double timeleft = Lag_Effect.get(uuid);
 					if (timeleft == 0) {
 						Lag.remove(uuid);
 					} else if (timeleft < 0) {
 						Lag.remove(uuid);
-
 					} else {
 						Player p = Bukkit.getPlayer(uuid);
 						p.teleport(p.getLocation());
 						Lag_Effect.replace(uuid, timeleft - 0.5);
 					}
-
 				}
-
 			}
-
 		}.runTaskTimer(this, 0, 10);
-
+		
 		new BukkitRunnable() {
-
 			@Override
 			public void run() {
 				if(Miner_Effect.isEmpty()) {return;}
 				for (UUID uuid : Miner_Effect.keySet()) {
-
 					Integer timeleft = Miner_Effect.get(uuid);
 					if (timeleft == 0) {
 						Miner_Effect.remove(uuid);
 					} else if (timeleft < 0) {
 						Miner_Effect.remove(uuid);
-
 					} else {
 						Player p = Bukkit.getPlayer(uuid);
 						Sound StoneBreak;
@@ -435,22 +416,17 @@ public class MainSpigot extends JavaPlugin implements Listener {
 						p.playSound(p.getLocation(), StoneBreak, 1, 10);
 						Miner_Effect.replace(uuid, timeleft - 1);
 					}
-
 				}
-
 			}
-
 		}.runTaskTimer(this, 0, 7);
+		
 		new BukkitRunnable() {
 			@Override
 			public void run() {
 				if(TempBan.isEmpty()) {return;}
 				for(UUID uuid : TempBan.keySet()) {
-
-
 					int timeleft = TempBan.get(uuid);
 					if (timeleft == 0) {
-
 						getBanFactory().removeTempBan(uuid);
 						if(getConfig().getBoolean("Force-Kick")) {
 							if(getServer().getPlayer(uuid)!= null) {
@@ -464,7 +440,7 @@ public class MainSpigot extends JavaPlugin implements Listener {
 							}
 						}
 					} else if (timeleft < 0) {
-						//						TempBan.remove(uuid);
+						// TempBan.remove(uuid);
 						getBanFactory().removeTempBan(uuid);
 						if(getConfig().getBoolean("Force-Kick")) {
 							if(getServer().getPlayer(uuid)!= null) {
@@ -480,28 +456,16 @@ public class MainSpigot extends JavaPlugin implements Listener {
 					} else {
 						TempBan.replace(uuid, timeleft - 1);
 					}
-
-
 				}
-
-
 			}
 		}.runTaskTimer(this, 0, 20);
 	}
-
-
+	
 	public BungeeCommunication getBungeeCommunication() {
 		return BungeeCommunication;
 	}
-
-
+	
 	public void setBungeeCommunication(BungeeCommunication bungeeCommunication) {
 		BungeeCommunication = bungeeCommunication;
 	}
-
-
-
-
-
-
 }
