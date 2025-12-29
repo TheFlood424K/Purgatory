0d17713d187908218cb16e0615bec5e5d5ae6793
Floods <80289503+TheFlood424K@users.noreply.github.com>
2025-12-29 17:02:33 -0500

    Fix unban broadcast showing Location object instead of message

--- src/main/java/ro/deiutzblaxo/Purgatory/Spigot/Commands/PurgeCommand.java
+++ src/main/java/ro/deiutzblaxo/Purgatory/Spigot/Commands/PurgeCommand.java
@@ -93,10 +93,8 @@ public class PurgeCommand extends Command {
 		}
 		plugin.getBanFactory().removeBan(player.getUniqueId());
 		plugin.getTaskFactory().removeTasks(player.getUniqueId());
-				String broadcastMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfigManager().getString
-				(plugin.getConfigManager().getMessages(),"Purge.broadcast")
-				.replaceAll("%player%", player.getName()).replaceAll("%admin%", sender.getName()));
-		for(Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+		String broadcastMessage = ChatColor.translateAlternateColorCodes('&', plugin.getConfigManager().getMessages().getString("Purge.broadcast"))
+				.replaceAll("%player%", player.getName()).replaceAll("%admin%", sender.getName());		for(Player onlinePlayer : Bukkit.getOnlinePlayers()) {
 			if(onlinePlayer.hasPermission("purgatory.staff")) {
 				onlinePlayer.sendMessage(broadcastMessage);
 			}
